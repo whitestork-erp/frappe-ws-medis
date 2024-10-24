e8dfb12813595e16a7fda5b62bebb073201c63d8
Rushabh Mehta <rmehta@gmail.com>
2024-10-24 15:21:34 +0530

    fix(style): fix oauth authorisation page and standardise error responses
    (cherry picked from commit cef8c12ee4c547460abca1d18355dbb45e4cba18)
    
    # Conflicts:
    #	frappe/app.py
    #	frappe/public/scss/desk/sidebar.scss
    #	frappe/public/scss/website/portal.scss
    #	frappe/www/error.html
    #	frappe/www/error.py
    #	frappe/www/message.html

--- frappe/www/error.py
+++ frappe/www/error.py
@@ -11,8 +11,19 @@ def get_context(context):
 	if frappe.flags.in_migrate:
 		return
 
+<<<<<<< HEAD
 	context.error_title = context.error_title or _("Uncaught Server Exception")
 	context.error_message = context.error_message or _("There was an error building this page")
+=======
+	allow_traceback = frappe.get_system_settings("allow_error_traceback") if frappe.db else False
+	if frappe.local.flags.disable_traceback and not frappe.local.dev_server:
+		allow_traceback = False
+
+	if not context.title:
+		context.title = _("Server Error")
+	if not context.message:
+		context.message = _("There was an error building this page")
+>>>>>>> cef8c12ee4 (fix(style): fix oauth authorisation page and standardise error responses)
 
 	return {
 		"error": frappe.get_traceback().replace("<", "&lt;").replace(">", "&gt;")
