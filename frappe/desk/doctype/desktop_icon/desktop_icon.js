541f8f3360ef909d1f8e5471ee6a93bc596cb175
sokumon <sohamkulkarns9@gmail.com>
2025-10-27 03:04:32 +0530

    fix(autocomplete): give Installed Applications as a option

--- frappe/desk/doctype/desktop_icon/desktop_icon.js
+++ frappe/desk/doctype/desktop_icon/desktop_icon.js
@@ -1,19 +1,8 @@
 // Copyright (c) 2016, Frappe Technologies Pvt. Ltd. and contributors
 // For license information, please see license.txt
 
-frappe.ui.form.on("Desktop Icon", {
-	setup: function (frm) {
-		load_installed_apps();
-	},
-});
+// frappe.ui.form.on("Desktop Icon", {
+// 	setup: function (frm) {
 
-async function load_installed_apps(frm) {
-	await frappe.call({
-		method: "frappe.desk.desktop.get_installed_apps",
-		callback: function (r) {
-			if (r.message) {
-				cur_frm.fields_dict["app"].set_data(r.message);
-			}
-		},
-	});
-}
+// 	},
+// });
