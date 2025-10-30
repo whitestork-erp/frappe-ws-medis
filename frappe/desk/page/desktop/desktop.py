f27d323c7bad9f02bcc000857d1073f08a1205a6
sokumon <sohamkulkarns9@gmail.com>
2025-10-30 15:48:00 +0530

    fix: show framework logo if brand logo is missing

--- frappe/desk/page/desktop/desktop.py
+++ frappe/desk/page/desktop/desktop.py
@@ -6,6 +6,11 @@ def get_context(context):
 	if frappe.session.user == "Guest":
 		frappe.local.flags.redirect_location = "/app"
 		raise frappe.Redirect
-	context.brand_logo = frappe.get_single_value("Navbar Settings", "app_logo")
+	brand_logo = None
+	brand_logo = frappe.get_single_value("Navbar Settings", "app_logo")
+	if not brand_logo:
+		brand_logo = frappe.get_hooks("app_logo_url", app_name="frappe")[0]
+	context.brand_logo = brand_logo
+	context.desktop_icons = get_desktop_icons()
 	context.current_user = frappe.session.user
 	return context
