15c9e8201c2b5dff917f3b7f505cbd1991a9d7d3
sokumon <sohamkulkarns9@gmail.com>
2025-11-06 23:06:05 +0530

    fix: semgrep issues

--- frappe/commands/site.py
+++ frappe/commands/site.py
@@ -1603,7 +1603,8 @@ def create_icons_and_sidebar(context: CliCtxObj):
 			create_desktop_icons()
 			print("Creating Workspace Sidebars")
 			create_workspace_sidebar_for_workspaces()
-			frappe.db.commit()
+			# Saving it in a command need it
+			frappe.db.commit()  # nosemgrep
 		except Exception as e:
 			print(f"Error creating icons {site}: {e}")
 		finally:
